9c30672a6d9b6a2e49f03e861902911f5688ff01
Vasuki-T <iamvasukithangaraj@gmail.com>
2026-07-13 22:22:48 +0530

    no changes

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
+app.use(cors({ origin: process.env.CLIENT_URL || '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
