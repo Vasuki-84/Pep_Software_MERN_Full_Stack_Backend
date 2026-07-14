e2e4c4b555c89acc017436a58112c498cb9a987d
Vasuki-T <iamvasukithangaraj@gmail.com>
2026-07-14 06:07:11 +0530

    Fix Railway server binding

--- src/server.js
+++ src/server.js
@@ -7,13 +7,12 @@ const PORT = process.env.PORT || 5000;
 // Connect to Database
 connectDB();
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on('unhandledRejection', (err) => {
   console.log(`Error: ${err.message}`);
-  // Close server & exit process
   server.close(() => process.exit(1));
 });
